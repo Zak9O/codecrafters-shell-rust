0e6261ed8274d9678f9ced5d2b2722b30800a2eb
Casper Koch <kochcasper@gmail.com>
2025-03-04 21:03:14 +1300

    done

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,53 @@
 use std::io::{self, Write};
 use std::process::exit;
 
+enum Cmd<'a> {
+    Exit(i32),
+    Echo(Vec<&'a str>),
+    Invalid,
+}
+
 fn main() {
     loop {
         print_leader();
         let input = get_user_input();
-        let input = input.trim();
-        if input == "exit 0" {
-            exit(0);
-        } else {
-            println!("{}: command not found", input.trim())
+        let input = handle_user_input(&input);
+        match input {
+            Cmd::Invalid => continue,
+            Cmd::Echo(args) => {
+                println!("{}", args.join(" "))
+            }
+            Cmd::Exit(x) => exit(x),
+        }
+    }
+}
+
+fn handle_user_input<'a>(input: &'a str) -> Cmd<'a> {
+    let mut iter = input.trim().split(' ');
+    let cmd = iter.next().unwrap();
+    let args: Vec<&str> = iter.collect();
+    match cmd {
+        "exit" => {
+            if args.len() == 0 || args.len() > 1 {
+                println!("exit expects one integer argument");
+                return Cmd::Invalid;
+            }
+            let exit_code: i32 = match args[0].parse() {
+                Err(_) => {
+                    println!("{} was expected to be an integer", args[0]);
+                    return Cmd::Invalid;
+                }
+                Ok(x) => x,
+            };
+
+            Cmd::Exit(exit_code)
+        },
+        "echo" => {
+            Cmd::Echo(args)
+        }
+        cmd => {
+            println!("{}: command not found", cmd);
+            Cmd::Invalid
         }
     }
 }
