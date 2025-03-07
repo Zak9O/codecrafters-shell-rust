f4c7906e9598525fe39f338c4a0c4b4974a704e8
Casper Koch <kochcasper@gmail.com>
2025-03-07 22:24:07 +1300

    done

--- src/cmd/custom.rs
+++ src/cmd/custom.rs
@@ -10,7 +10,7 @@ impl<'a> Cmd for Custom<'a> {
             .args(&self.1)
             .output()
             .expect("Failed executing {exec_path}");
-        println!("{}", String::from_utf8(output.stdout).unwrap())
+        print!("{}", String::from_utf8(output.stdout).unwrap())
     }
 }
 
