b3c9bbe26ff84bdb4ee7fab66b1d801b94ee98d0
Casper Koch <kochcasper@gmail.com>
2025-03-07 22:34:35 +1300

    done

---
+++ src/cmd/pwd.rs
@@ -0,0 +1,19 @@
+use std::env::current_dir;
+
+use super::Cmd;
+
+pub struct Pwd;
+
+impl Cmd for Pwd {
+    fn execute(&self) -> () {
+        let binding = current_dir().unwrap();
+        let pwd = binding.to_str().unwrap();
+        println!("{pwd}");
+    }
+}
+
+impl Pwd {
+    pub fn new() -> Self {
+        Self
+    }
+}
